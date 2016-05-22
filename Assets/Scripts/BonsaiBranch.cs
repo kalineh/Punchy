0312cbc0a17aeb48d3fcaf35040109aa7f0152ce
kalin.eh <kalin.eh@gmail.com>
2016-05-21 21:19:23 -0700

    branching test

---
+++ Assets/Scripts/BonsaiBranch.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BonsaiBranch
+    : MonoBehaviour
+{
+    private GameObject branches;
+    private GameObject stalk;
+    private GameObject leaf;
+
+    private Collider stalkCollider;
+    private Collider leafCollider;
+
+    private Renderer stalkRenderer;
+    private Renderer leafRenderer;
+
+    private int depth = 0;
+
+    public void Start()
+    {
+        OnScriptReload();
+    }
+
+    public void OnScriptReload()
+    {
+        branches = transform.FindChild("Branches").gameObject;
+        stalk = transform.FindChild("Stalk").gameObject;
+        leaf = transform.FindChild("Leaf").gameObject;
+
+        stalkCollider = stalk.GetComponent<Collider>();
+        leafCollider = leaf.GetComponent<Collider>();
+
+        stalkRenderer = stalk.GetComponent<Renderer>();
+        leafRenderer = leaf.GetComponent<Renderer>();
+
+        StartCoroutine(DoGrow());
+    }
+
+    public GameObject MakeBranch()
+    {
+        var depthChild = depth + 1;
+        if (depthChild > 20)
+            return null;
+
+        var prefab = Resources.Load<GameObject>("BonsaiBranch");
+        var obj = GameObject.Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
+        var ofs = Vector3.RotateTowards(Vector3.up, Random.onUnitSphere, 1.25f, 0.0f);
+        var closest = leafCollider.ClosestPointOnBounds(ofs);
+        var scale = 1.0f - depth * 0.075f;
+
+        obj.transform.SetParent(branches.transform);
+        obj.transform.position = closest;
+        obj.transform.localRotation = Quaternion.LookRotation(ofs.normalized);
+        obj.transform.localScale = Vector3.one * scale;
+
+        var bonsai = obj.GetComponent<BonsaiBranch>();
+
+        bonsai.name = string.Format("Branch{0}", depthChild);
+        bonsai.depth = depthChild;
+
+        return obj;
+    }
+
+    public IEnumerator DoGrow()
+    {
+        yield return new WaitForSeconds(Random.Range(1.0f, 5.0f) * depth);
+
+        while (true)
+        {
+            var depthDelay = depth * 1.0f;
+            var grownDelay = branches.transform.childCount * (depth + 1) * 5.0f;
+
+            yield return new WaitForSeconds(depthDelay + grownDelay);
+
+            MakeBranch();
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/DecoSphere.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DecoSphere
+    : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private float shuffle;
+
+    public void Start()
+    {
+        OnScriptReload();
+    }
+
+    public void OnScriptReload()
+    {
+        startPosition = transform.position;
+        shuffle = Random.Range(0.0f, 30.0f);
+        transform.localScale = transform.localScale * Random.Range(0.7f, 1.3f);
+    }
+
+	void Update()
+    {
+        var t = Time.time + shuffle;
+        var p = new Vector3(
+            Mathf.Sin(t * 0.13f) * 0.04f,
+            Mathf.Sin(t * 0.16f) * 0.3f,
+            Mathf.Sin(t * 0.12f) * 0.06f
+        );
+
+        transform.position = startPosition + p;
+	}
+}
